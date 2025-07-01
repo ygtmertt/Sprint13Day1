44a60d6a253b415682d33b3eba4dc7b92ef71c4c
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2025-07-01 09:31:41 +0300

    shouldWakeUp function completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,25 +3,36 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(shouldWakeUp(true, 1));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if(clock > 23 || clock < 0) {
+            return false;
+        }
+        else {
+            if(clock > 20 || clock < 8 && isBarking) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
-    }
-
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
-    }
-
-    public static double area(double width, double height) {
-       
-    }
-
-    public static double area(double radius) {
-       
-    }
+//    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+//
+//    }
+//
+//    public static boolean isCatPlaying(boolean isSummer, int temp) {
+//
+//    }
+//
+//    public static double area(double width, double height) {
+//
+//    }
+//
+//    public static double area(double radius) {
+//
+//    }
 }
