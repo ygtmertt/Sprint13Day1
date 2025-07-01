f49294930ded5ac75b0cf15827b6436846fc8112
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2025-07-01 09:53:33 +0300

    hasTeen function completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,36 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(shouldWakeUp(true, 1));
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println(shouldWakeUp(true, 1));
+    System.out.println(hasTeen(22, 23, 34));
+  }
 
-    public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        if(clock > 23 || clock < 0) {
-            return false;
-        }
-        else {
-            if(clock > 20 || clock < 8 && isBarking) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+  public static boolean shouldWakeUp(boolean isBarking, int clock) {
+    if(clock > 23 || clock < 0) {
+      return false;
     }
+    else {
+      if(clock > 20 || clock < 8 && isBarking) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  }
 
-//    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-//
-//    }
+  public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+    if(firstAge<=19 && firstAge>=9 ||
+        secondAge<=19 && secondAge>=9 ||
+        thirdAge<=19 && thirdAge>=9) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 //
 //    public static boolean isCatPlaying(boolean isSummer, int temp) {
 //
